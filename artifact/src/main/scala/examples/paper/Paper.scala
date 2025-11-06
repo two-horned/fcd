6fecca5d34419fcbc43cf19dacd5cac13b366602
Said Kadrioski <said@kadrioski.de>
2025-11-06 14:40:02 +0100

    Migrate Paper.scala

--- artifact/src/main/scala/examples/paper/Paper.scala
+++ artifact/src/main/scala/examples/paper/Paper.scala
@@ -1,16 +1,13 @@
 package fcd
 
-/**
- * This object instantiates the examples from section 3, 4 and 7 and makes them
- * available in the REPL via:
- *
- * > import paper._
- */
-object paper extends Section3 with Section4 with Section7 {
-
-  // Use the derivative based parsers for examples in the paper
-  type Parsers = DerivativeParsers.type
-  def _parsers: DerivativeParsers.type = DerivativeParsers
-  override lazy val parsers: DerivativeParsers.type = _parsers
-
-}
+/** This object instantiates the examples from section 3, 4 and 7 and makes them
+  * available in the REPL via:
+  *
+  * > import paper._
+  */
+object paper
+    extends RichParsers
+    with DerivativeParsers
+    with Section3
+    with Section4
+    with Section7
