73799b5b12b7e9d638f975a7ed199f2e0b4d8399
Said Kadrioski <said@kadrioski.de>
2026-03-22 21:28:35 +0100

    Use view to avoid duplicate list creation

--- artifact/src/main/scala/library/DerivativeParsers.scala
+++ artifact/src/main/scala/library/DerivativeParsers.scala
@@ -25,7 +25,7 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     def not: Parser[Unit] = Not(p)
 
     // the map family
-    infix def mapResults[U](f: (=> Results[R]) => Results[U]): Parser[U] =
+    infix def mapResults[U](f: Results[R] => Results[U]): Parser[U] =
       MapResults(p, f)
     infix def map[U](f: R => U): Parser[U] = p.mapResults(_.map(f))
     infix def withResults[U](res: List[U]): Parser[U] = mapResults(_ => res)
@@ -48,7 +48,7 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     override def and[U](q: Parser[U]) = this
     override def map[U](f: Nothing => U) = this
     override def flatMap[U](g: Nothing => Parser[U]) = this
-    override def mapResults[U](f: (=> Results[Nothing]) => Results[U]) = this
+    override def mapResults[U](f: Results[Nothing] => Results[U]) = this
     override def done = this
 
     override def not = Always
@@ -78,7 +78,7 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     override def consume(x: Elem) = fail
     override def toString = s"ε($ress)"
     override def done = this
-    override def mapResults[T](f: (=> Results[R]) => Results[T]) =
+    override def mapResults[T](f: Results[R] => Results[T]) =
       Succeed(f(ress))
     override def seq[U](q: Parser[U]) = q mapResults { ress2 =>
       for {
@@ -165,7 +165,7 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     override def toString = s"done($p)"
   }
 
-  class MapResults[R, U](val p: Parser[R], f: (=> Results[R]) => Results[U])
+  class MapResults[R, U](val p: Parser[R], f: Results[R] => Results[U])
       extends UnaryPrintable(s"mapResults", p)
       with Parser[U] {
     // preserve whether p actually has results (f might ignore its argument...)
@@ -173,9 +173,9 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     def failed = p.failed
     def accepts = p.accepts
     def consume(x: Elem) = (p consume x) mapResults f
-    override def mapResults[T](g: (=> Results[U]) => Results[T]) =
+    override def mapResults[T](g: Results[U] => Results[T]) =
       p mapResults { res => g(f(res)) }
-    override def map[T](g: U => T) = p mapResults { res => f(res) map g }
+    override def map[T](g: U => T) = p mapResults { f(_) map g }
     override def done = p.done mapResults f
 
     // we can forget the results here.
@@ -185,13 +185,13 @@ trait DerivativeParsers extends Parsers { self: DerivedOps =>
     // canonicalization rule (2) from PLDI 2016
     // allows for instance rewriting (always.map(f) & p) -> p.map(...f...)
     override def seq[S](q: Parser[S]) =
-      (p seq q).mapResults(rss =>
-        rss.unzip match { case (us, ss) => f(us) zip ss }
-      )
+      (p seq q).mapResults(_.view.unzip match {
+        case (us, ss) => f(List.from(us)) zip ss
+      })
     override def and[S](q: Parser[S]) =
-      (p and q).mapResults(rss =>
-        rss.unzip match { case (us, ss) => f(us) zip ss }
-      )
+      (p and q).mapResults(_.view.unzip match {
+        case (us, ss) => f(List.from(us)) zip ss
+      })
   }
 
   class And[R, U](val p: Parser[R], val q: Parser[U])
